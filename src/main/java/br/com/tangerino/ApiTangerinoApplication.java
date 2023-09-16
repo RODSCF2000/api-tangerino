5a43bb7c8de9c4d5eab56f7537845d71b622f2ea
Rodrigo <rodrigo.c2000@hotmail.com>
2023-09-16 13:47:23 -0300

    first commit

---
+++ src/main/java/br/com/tangerino/ApiTangerinoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tangerino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTangerinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTangerinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tangerino/ApiTangerinoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tangerino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTangerinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
